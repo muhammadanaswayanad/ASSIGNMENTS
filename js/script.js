c6272d895fad0f6c5c5792949223fb13046f417b
muhammadanaswayanad <muhammadanaswayanad@gmail.com>
2017-03-18 15:17:11 +0530

    :))

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 function msg() {
-    alert("Hello !!!");
+    name = document.form.name.value;
+    alert(name);
 }
 
 function my_func() {
-    var a = "String";
-    alert(a);
-    alert("fg")
+    var limit = document.form.limitbox.value;
+    for (var i = 0; i <= limit; i++) {
+        if (i % 2 == 0) {
+            document.write(" " + i);
+        }
+    }
+}
+
+function sum() {
+    var a = document.form.num1.value;
+    var b = document.form.num2.value;
+    c = a + b;
+    document.write(c);
+}
+
+function check() {
+    s = document.forms[1].rad1.value;
+    alert(s);
 }
